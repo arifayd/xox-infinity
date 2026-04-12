097e38081d61c9a3039e680bb2b5ec0c401c22d6
Hacı Arif Aydoğdu <aydogdu.arif@hotmail.com>
2026-04-12 14:28:00 +0300

    Guncelleme notu

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,18 +5,5 @@ const FIREBASE_CONFIG = {
   storageBucket: "xoxinfinity-96dce.firebasestorage.app",
   messagingSenderId: "274803484143",
   appId: "1:274803484143:web:927891ef9abeb051c1021a",
-  // Google Client ID buraya geri geldi:
   googleClientId: "274803484143-8vr4gfje3volompe4jg43gbaglh4r7i4.apps.googleusercontent.com"
 };
-
-// Firebase Başlatma
-if (!firebase.apps.length) {
-    firebase.initializeApp(FIREBASE_CONFIG);
-}
-
-const auth = firebase.auth();
-const db = firebase.firestore();
-
-// Google Sağlayıcı Ayarı
-const googleProvider = new firebase.auth.GoogleAuthProvider();
-// Eğer login fonksiyonunda googleClientId kullanıyorsan bu değişkeni orada kullanabilirsin.
